760005c9583a0b822c6ce7514438cad94239bded
Lito <lito@eordes.com>
2015-12-09 14:05:01 +0100

    Updated templates

--- src/templates/layout/base.php
+++ src/templates/layout/base.php
@@ -4,7 +4,7 @@
         <?php template()->show('molecules.head'); ?>
     </head>
 
-    <body class="body-{{ str_replace('.', '-', $ROUTE) }}">
+    <body class="body-<?= str_replace('.', '-', $ROUTE); ?>">
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
