812f64002e353768b1906eeab5b79b2dcaec7ddf
Lito <lito@eordes.com>
2015-12-10 13:30:20 +0100

    Updated git class function scope

--- src/WebDeploy/Processor/Admin.php
+++ src/WebDeploy/Processor/Admin.php
@@ -6,7 +6,7 @@ use WebDeploy\Shell\Shell;
 
 class Admin extends Git
 {
-    private function exec($cmd)
+    protected function exec($cmd)
     {
         return (new Shell(Route::getBasePath()))->log(true)->exec($cmd)->getLogs()[0];
     }
