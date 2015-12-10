dc1dcbbd950ecc64ea91acc4625815daba6f60e0
Lito <lito@eordes.com>
2015-12-10 19:09:07 +0100

    Added composer update to admin

--- src/WebDeploy/Processor/Admin.php
+++ src/WebDeploy/Processor/Admin.php
@@ -3,11 +3,36 @@ namespace WebDeploy\Processor;
 
 use WebDeploy\Repository;
 use WebDeploy\Router\Route;
+use WebDeploy\Shell\Shell;
 
 class Admin extends Git
 {
+    public function update()
+    {
+        if (!$this->check() || !($processor = input('processor'))) {
+            return null;
+        }
+
+        switch ($processor) {
+            case 'composer-install':
+                return array($processor => $this->composerInstall());
+        }
+
+        return parent::update();
+    }
+
     protected function exec($function, $parameter = null)
     {
         return (new Repository\Git(Route::getBasePath(), true))->$function($parameter)->getShellAndLog();
+    }
+
+    protected function composerInstall()
+    {
+        $path = Route::getBasePath();
+
+        return (new Shell($path))
+            ->baseCommand('export COMPOSER_HOME="'.$path.'";')
+            ->exec('composer install')
+            ->getLog();
     }
 }
