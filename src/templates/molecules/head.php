4e84f0aef4869ab6173f660001702ae4961c93cc
Lito <lito@eordes.com>
2015-12-11 17:06:51 +0100

    Added assets packer

--- src/templates/molecules/head.php
+++ src/templates/molecules/head.php
@@ -7,7 +7,9 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="<?= asset('/css/bootstrap-docs.min.css'); ?>" />
-<link rel="stylesheet" href="<?= asset('/css/custom.css'); ?>" />
+<?= packer()->css([
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
+    '/assets/css/bootstrap-docs.min.css',
+    '/assets/css/custom.css'
+], '/assets/cache/css/styles.css'); ?>
