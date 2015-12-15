05a6bd03dc422af33939f51c5eaff25042861b2c
Lito <lito@eordes.com>
2015-12-15 13:40:30 +0100

    Updated rsync module

---
+++ src/templates/pages/rsync/update.php
@@ -0,0 +1,77 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?= __('Looking for changed files'); ?></h3>
+    </div>
+
+    <div class="panel-body">
+        <form method="post">
+            <div class="form-group">
+                <button type="submit" name="find" value="true" class="btn btn-default form-control">
+                    <?= __('Find'); ?>
+                </button>
+            </div>
+
+            <?php if (isset($processor['rsync-upload'])) { ?>
+
+            <?php if (isset($processor['rsync-upload']['error'])) { ?>
+
+            <pre class="bg-danger"><?= $processor['rsync-upload']['error']; ?></pre>
+
+            <?php } else { ?>
+
+            <?php foreach ($processor['rsync-upload']['success'] as $row) { ?>
+
+            <pre class="bg-<?= $row['status'] ? 'success' : 'danger'; ?>"><strong><?= $row['cmd']; ?>:</strong> <?= $row['arguments']; ?></pre>
+
+            <?php } ?>
+
+            <?php } ?>
+
+            <?php } ?>
+
+            <?php if (input('find') === 'true') { ?>
+
+            <?php if (empty($files)) { ?>
+
+            <div class="alert alert-success"><?= __('There are not modified files'); ?></div>
+
+            <?php } else { ?>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th><input type="checkbox" data-checkall=".table" checked /></th>
+                        <th><?= __('Name'); ?></th>
+                        <th><?= __('Date'); ?></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php foreach ($files as $file) { ?>
+                    <tr>
+                        <td><input type="checkbox" id="file-<?= $file['code']; ?>" name="files[]" value="<?= $file['code']; ?>" checked /></td>
+                        <td><label for="file-<?= $file['code']; ?>"><?= $file['name']; ?></label></td>
+                        <td><?= date('Y-m-d H:i:s', $file['date']); ?></td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+
+                <tfoot>
+                    <tr>
+                        <th><input type="checkbox" data-checkall=".table" checked /></th>
+                        <th><?= __('Name'); ?></th>
+                        <th><?= __('Date'); ?></th>
+                    </tr>
+                </tfoot>
+            </table>
+
+            <button type="submit" name="processor" value="rsync-upload" class="btn-success form-control">
+                <?= __('Upload to %s', $config['host']); ?>
+            </button>
+
+            <?php } ?>
+
+            <?php } ?>
+        </form>
+    </div>
+</div>
